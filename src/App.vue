1f00591965542bfa61be8a09f4084fab89a68cbb
zjy <296579033@qq.com>
2018-07-24 15:14:54 +0800

    首页页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,154 @@
 <template>
   <div id="app">
+    <div class="menu">
+      <el-menu :default-active="$route.path" router class="el-menu-demo" mode="horizontal">
+        <li class="menu-logo">
+          <img src="../static/img/apxq_head_portrait@2x.png" class="menu-icon">
+        </li>
+        <div class="text-center">
+          <ul>
+        <el-menu-item index="/" class="menu-box">首页</el-menu-item>
+        <el-submenu index="1">
+          <template slot="title">金融服务</template>
+          <el-menu-item index="SmallSupermarket">小额超市</el-menu-item>
+          <el-menu-item index="CreditManager">信贷经理</el-menu-item>
+          <el-menu-item index="BankLoan">银行借贷</el-menu-item>
+          <el-menu-item index="CardSupermarket">信用卡超市</el-menu-item>
+          <el-menu-item index="CarSupermarket">车险超市</el-menu-item>
+          <el-menu-item index="POSSupermarket">POS超市</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">帮助说明</template>
+          <el-menu-item index="Flow">流程说明</el-menu-item>
+          <el-menu-item index="formmanual">平台手册</el-menu-item>
+          <el-menu-item index="Help">帮助中心</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">查询服务</template>
+          <el-menu-item index="CardInquiry">信用卡查询</el-menu-item>
+          <el-menu-item index="Loaninquiry">贷款查询</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="Find">发现</el-menu-item>
+        <el-button class="menu-btn" size="small">退出</el-button>
+        <img src="../static/img/apxq_head_portrait@2x.png" class="menu-icon">
+        </ul>
+        </div>
+        
+      </el-menu>
+    </div>
     <router-view/>
+    <div class="foot ">
+      <div class="container">
+      <el-row :gutter="10">
+      <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">客服热线：400-800-800 周一至周六（9:00-18:00）</el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">公司地址：江苏省无锡市太湖b区2幢905</el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8"><span style="font-family:Arial;">©</span>2002-2017. 无锡太湖有限公司. 版权所有.</el-col>
+      </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+
 </script>
 
 <style>
+  html,
+  body {
+    width: 100%;
+    margin: 0;
+  }
+
+  .el-menu-demo.el-menu--horizontal.el-menu {
+    border-bottom: unset;
+    margin-right: auto;
+    margin-left: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+  @media (min-width: 768px) {
+    .el-menu-demo.el-menu--horizontal.el-menu {
+      width: 750px;
+    }
+    .menu-logo {
+        float: left;
+        margin-right: 0;
+        margin-left: 0
+    }
+  }
+  .menu-logo {
+    margin-right: -15px;
+    margin-left: -15px
+  }
+  @media (min-width: 992px) {
+    .el-menu-demo.el-menu--horizontal.el-menu {
+      width: 970px;
+    }
+  }
+  @media (min-width: 1200px) {
+    .el-menu-demo.el-menu--horizontal.el-menu {
+      width: 1170px;
+    }
+  }
+
+  .text-center{
+    text-align: center;
+  }
+  .text-center>ul>li{
+    display: inline-block;
+  }
+  .el-submenu__icon-arrow {
+    position: unset;
+    margin-top: 0;
+    margin-left: 14px;
+}
+
+  .menu-name,
+  .menu-logo {
+    height: 60px;
+    line-height: 60px;
+    margin: 0;
+    float: left;
+  }
+
+  .menu-icon {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    float: right;
+    margin-right: 20px;
+  }
+
+  .menu-btn {
+    float: right;
+    margin-right: 20px;
+    margin-top: 10px;
+  }
+
+  .foot {
+    background-color: #3B3B3B;
+    color: #fff;
+    margin: 0;
+    padding: 50px 30px;
+  }
+
+  .foot .foot-text+.foot-text {
+    margin-left: 120px;
+  }
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,60 +1,285 @@
 <template>
-  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">首页</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">金融服务</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-submenu index="3">
-    <template slot="title">帮助说明</template>
-    <el-menu-item index="3-1">选项1</el-menu-item>
-    <el-menu-item index="3-2">选项2</el-menu-item>
-    <el-menu-item index="3-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-submenu index="4">
-    <template slot="title">资讯服务</template>
-    <el-menu-item index="4-1">选项1</el-menu-item>
-    <el-menu-item index="4-2">选项2</el-menu-item>
-    <el-menu-item index="4-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-submenu index="5">
-    <template slot="title">查询服务</template>
-    <el-menu-item index="5-1">选项1</el-menu-item>
-    <el-menu-item index="5-2">选项2</el-menu-item>
-    <el-menu-item index="5-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3">发现</el-menu-item>
-  <img src="../../static/img/apxq_head_portrait@2x.png" class="menu-icon">
-</el-menu>
+  <div>
+    <el-carousel indicator-position="none" arrow="always" height="500px">
+      <el-carousel-item v-for="item in bannerlist" :key="item.url">
+        <img :src="item.url" class="banner-img">
+      </el-carousel-item>
+    </el-carousel>
+    <h3>
+      <span class="product-title">推荐产品</span>
+    </h3>
+    <div class="container">
+      <el-row :gutter="10">
+        <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+          <el-card class="box-card" shadow="hover">
+            <div class="header">
+              <span>宜人贷</span>
+            </div>
+            <div class="body">
+              <p class="card-money">10000</p>
+              <p class="card-title">最高额度</p>
+              <el-tag v-for="tag in tags" :key="tag.name">
+                {{tag.name}}
+              </el-tag>
+              <el-button type="primary" class="card-btn">免费申请</el-button>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+          <el-card class="box-card" shadow="hover">
+            <div class="header">
+              <span>宜人贷</span>
+            </div>
+            <div class="body">
+              <p class="card-money">10000</p>
+              <p class="card-title">最高额度</p>
+              <el-tag v-for="tag in tags" :key="tag.name">
+                {{tag.name}}
+              </el-tag>
+              <el-button type="primary" class="card-btn">免费申请</el-button>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+          <el-card class="box-card" shadow="hover">
+            <div class="header">
+              <span>宜人贷</span>
+            </div>
+            <div class="body">
+              <p class="card-money">10000</p>
+              <p class="card-title">最高额度</p>
+              <el-tag v-for="tag in tags" :key="tag.name">
+                {{tag.name}}
+              </el-tag>
+              <el-button type="primary" class="card-btn">免费申请</el-button>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="dark">
+      <h3>
+        <span class="product-title advantage">六大优势</span>
+      </h3>
+      <div class="container">
+        <el-row :gutter="10">
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8">
+            <el-card class="box-card" shadow="hover">
+              <div class="body">
+                <img src="../../static/img/apxq_head_portrait@2x.png" class="advantage-icon">
+                <p>高校专业</p>
+                <p class="advantage-text">优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势优势</p>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <h3>
+      <span class="product-title">关于我们</span>
+    </h3>
+    <div class="container">
+<p class="aboutus">
+  金融 指货币的发行、流通和回笼，贷款的发放和收回，存款的存入和提取，汇兑的往来等经济活动。<br/>
+  金融（FINANCE）就是对现有资源进行重新整合之后，实现价值和利润的等效流通。（专业的说法是：实行从储蓄到投资的过程，狭义的可以理解为金融是动态的货币经济学。）<br/>
+金融是人们在不确定环境中进行资源跨期的最优配置决策的<br/>
+金融的本质是价值流通。金融产品的种类有很多，其中主要包括银行、证券、保险、信托等。金融所涉及的学术领域很广，其中主要包括：会计、财务、投资学、银行学、证<br/>
+券学、保险学、信托学等等。<br/>
+金融是一种交易活动，金融交易本身并未创造价值，那为什么在金融交易中就有赚钱的呢？按照陈志武先生的说法，金融交易是一种将未来收入变现的方式，也就是明天的钱<br/>
+今天来花。<br/>
+简单地说金融交易的频繁程度就是反映一个地区、区域、乃至国家经济繁荣能力的重要指标。
+</p>
+    </div>
+  </div>
+
 
 </template>
 
 <script>
-export default {
+  export default {
     data() {
       return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
+        bannerlist: [{
+          url: "../../static/img/banner.png"
+        }, {
+          url: "../../static/img/1920x896.jpg"
+        }],
+        tags: [{
+            name: '标签一'
+          },
+          {
+            name: '标签二'
+          },
+          {
+            name: '标签三'
+          },
+          {
+            name: '标签四'
+          },
+          {
+            name: '标签五'
+          }
+        ]
       }
     }
   }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.el-menu--horizontal{
-  float: right;
-}
-.menu-icon{
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
+  /* banner */
+  .banner-img {
+    width: 100%;
+    height: 100%;
+  }
+
+  /* 推荐产品 */
+
+  h3 {
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  .product-title {
+    border-bottom: 3px solid #CEAA70;
+    padding: 0 10px 10px 10px;
+  }
+
+  .el-row {
+    margin-top: 60px;
+  }
+
+  .el-card {
+    width: 96%;
+    margin-left: 2%;
+    margin-top: 20px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .header {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  .body {
+    text-align: center;
+  }
+
+  .card-money {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 30px 0 0 0 !important;
+  }
+
+  .card-title {
+    color: #BBBBBB;
+    font-size: 15px;
+    margin: 0 !important;
+  }
+
+  .body .el-tag+.el-tag {
+    margin-left: 10px;
+  }
+
+  .el-tag {
+    margin-top: 20px;
+    background-color: #fff;
+    color: #CEAA70;
+    border: 1px solid #CEAA70;
+  }
+
+  .card-btn {
+    width: 80%;
+    margin-top: 50px;
+  }
+
+  /* 优势 */
+
+  .dark {
+    background: url(../../static/img/1.gif);
+    color: #eee;
+    background-size: 100% 100%;
+    padding: 80px 0;
+    margin-top: 50px;
+  }
+
+  .advantage-icon {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .advantage-text {
+    text-align: left
+  }
+
+  .advantage{
+    color:#fff;
+    border-bottom: 3px solid #fff;
+  }
+  .dark h3{
+    margin-top: 0;
+  }
+  .dark .el-card {
+    background-color:unset;
+    color: #fff;
+  }
+  .aboutus{
+    font-size: 18px;
+    font-family: MicrosoftYaHei;
+    color: rgba(102,102,102,1);
+    line-height: 25px;
+    margin-top: 50px;
+    padding-bottom: 200px;
+  }
+
 </style>
